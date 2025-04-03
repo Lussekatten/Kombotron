2df0ddf249204a4cd7790c5baa4c1ff856824e0e
Lucian Ralea <lucian.ralea@gmail.com>
2025-04-03 13:35:06 +0200

    JSON file created and uploaded to UPLOADTHINGS cloudstorage. Trying to figure out how to read the contents into my application.

---
+++ app/actions.ts
@@ -0,0 +1,10 @@
+// import path from "path"
+
+// export async function getStaticFile(file) {
+//     let basePath = process.cwd()
+
+
+//     const filePath = path.join(basePath, `file`)
+//     const fileContent = await fs.readFile(filePath, "utf8")
+//     return fileContent
+// }
