a5ddfccf5143fcf0b0c6f74fed2e45803197af76
Lucian Ralea <lucian.ralea@gmail.com>
2025-04-04 09:14:39 +0200

    Justerade lite responsiviteten för småskärmar

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -1,5 +1,21 @@
-export default function IntroPage(){
-    return(
-        <h1>Info sidan</h1>
+import { Roboto, Geist_Mono } from "next/font/google";
+import styles from './intro.module.css';
+
+
+const roboto = Roboto({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export default function IntroPage() {
+    return (
+        <main className={styles.allWrapper}>
+            <h1 className={`${roboto.variable}`}>Intro sidan</h1>
+        </main>
     );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TopNavMenu from "./components/navbar/menus";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Kombotron",
   description: "Lotto, svenska lotteriet, kombinationer, lottosystem",
@@ -25,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <TopNavMenu />
         {children}
       </body>
