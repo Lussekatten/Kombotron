2b1515b2398aa4c426a2de5a32631732c075a8dd
Lucian Ralea <lucian.ralea@gmail.com>
2025-04-15 05:15:06 +0200

    Fixed some layout issues with the winnings table.

--- app/stats/grouping/page.tsx
+++ app/stats/grouping/page.tsx
@@ -1,10 +1,11 @@
 import { StatsNavMenu } from "@/app/components/navbar/menus";
+import styles from "./grouping.module.css"
 
 export default function GroupingStatsPage() {
     return (
-        <>
+        <div className={styles.allWrapper}>
             <StatsNavMenu />
             <h1>Statistik om grupperingar</h1>
-        </>
+        </div>
     );
 }
